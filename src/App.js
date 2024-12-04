08e7dc96fbf8659f37c2c5458982e593a8f580c3
Andrew Mason <andrewscottmason@gmail.com>
2024-12-03 19:27:49 -0500

    Updated App.js with custom content

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Taskwatch</h1>
+      {/* Add your custom components here */}
+    </div>
+  );
+}
+
+export default App;
